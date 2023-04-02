b9dac08e34e517e62f3acc1a6874b0fa8a0ff7eb
Siquiniki <danielahmedmaya@gmail.com>
2023-04-02 20:23:05 +0200

    subuer

---
+++ js/app-main.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function(){
+    var navbar = document.querySelector('.nav-scrolled');
+
+    if( this.window.screenY > 300 ){
+        navbar.getElementsByClassNam
+    } else {
+        navbar.classList.remove('navbar');
+    }
+});
